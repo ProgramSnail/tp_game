8de85dcd620017b9d8c1f3b341de8c3c77a85557
ProgramSnail <purpurebacterium@yandex.ru>
2021-03-21 17:36:43 +0300

    hello world ncurses test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <curses.h>
 #include <cstdlib>
+#include <unistd.h>
 
 int main() {
     initscr();
     noecho();
+    mvprintw(0, 0, "Hello World!");
+    refresh();
+    sleep(1);
     curs_set(false);
     endwin();
 }
